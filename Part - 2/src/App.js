939af09f2ce813ee7379c52c757860886e845a6e
Neel Patel <neelp@metropolia.fi>
2023-02-12 17:10:31 +0200

    - Part 0 , 1 and 2 added

---
+++ Part - 2/src/App.js
@@ -0,0 +1,112 @@
+// import React, { useState } from 'react'
+// import ReactDOM from 'react-dom'
+
+
+// const Header = props => <h1>{props.name}</h1>
+
+// const Statistic = ({text, value}) => {
+//   if (text === "positive") {
+//     return (
+//       <tr><td>{text} {value} %</td></tr>
+//     )
+//   }
+  
+//   return (
+//     <tr><td>{text} {value}</td></tr>
+//   )
+// }
+
+// const Statistics = ({clicks}) => {
+//     const total = clicks.good + clicks.neutral + clicks.bad
+//     const average = (clicks.good * 1 + clicks.bad * -1) / total
+//     const positive = clicks.good * (100/total)
+
+//     if (total === 0) {
+//       return (
+//         <div>
+//           No feedback given
+//         </div>
+//       )
+//     }
+
+//     return (
+//       <div>
+//         <table>
+//           <tbody>
+//             <Statistic text="good" value={clicks.good} />
+//             <Statistic text="neutral" value={clicks.neutral} />
+//             <Statistic text="bad" value={clicks.bad} />
+//             <Statistic text="all" value={total} />
+//             <Statistic text="average" value={average} />
+//             <Statistic text="positive" value={positive} />
+//           </tbody>
+//         </table>
+//       </div>
+//     )
+// }
+
+
+// const Button = ({ onClick, text }) => (
+//   <button onClick={onClick}>
+//     {text}
+//   </button>
+// )
+
+// const App = () => {
+//   // save clicks of each button to own state
+//   const [clicks, setClicks] = useState({
+//     good: 0, neutral: 0, bad: 0
+//   })
+
+//   const handleGoodClick = () =>
+//     setClicks({...clicks, good: clicks.good + 1})
+
+//   const handleNeutralClick = () =>
+//     setClicks({...clicks, neutral: clicks.neutral + 1})
+
+//   const handleBadClick = () =>
+//     setClicks({...clicks, bad: clicks.bad + 1})
+
+//   return (
+//     <div>
+//       <Header name="Customer feedback" />
+//       <Button onClick={handleGoodClick} text='good' />
+//       <Button onClick={handleNeutralClick} text='neutral' />
+//       <Button onClick={handleBadClick} text='bad' />
+//       <Header name="Statistics" />
+//       <Statistics clicks={clicks} />
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App />,
+//   document.getElementById('root')
+// )
+
+/* function ListItem(props) {
+  const value = props.value;
+  return (
+    <li>
+      {value.toString()}
+    </li>
+  );
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <ListItem value={number} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+); */
+
